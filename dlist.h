723a99a109e9cc881c826efccf16207975370431
JonathanLoza <lozajnt@gmail.com>
2018-09-07 19:42:21 -0500

    Fin de develop

--- dlist.h
+++ dlist.h
@@ -4,58 +4,114 @@
 #include "iterator.h"
 
 template <typename T>
-class DListIterator : public Iterator<T> {     
-    public: 
+class DListIterator : public Iterator<T> {
+    public:
         DListIterator() : Iterator<T>() {};
-        DListIterator(Node<T> *current) : Iterator<T>(current) {};
-        DListIterator<T> operator++();
-        DListIterator<T> operator--();
+        DListIterator(Node<T> *current) : Iterator<T>(current) {
+          //this->current=current;
+        };
+        DListIterator<T> operator++(){
+          this->current=this->current->next;
+          return *this;
+        };
+        DListIterator<T> operator--(){
+          this->current=this->current->prev;
+          return *this;
+        };
 };
 
 template <typename Tr>
-class DList {     
+class DList {
     public:
         typedef typename Tr::T T;
         typedef DListIterator<T> iterator;
-      
+
     private:
         Node<T>* head;
         Node<T>* tail;
-              
+        int nodes;
     public:
         DList() {
             head = tail = nullptr;
+            nodes=0;
         };
 
         void push_front(T data) {
+          auto* temp= new Node<T>(data);
+          if (head==nullptr) {
+              head=temp;
+              tail=temp;
+          }else{
+              temp->next=head;
+              head->prev=temp;
+              head=temp;
+          }
+          nodes++;
             // TODO
         }
 
         void push_back(T data) {
+          auto* temp= new Node<T>(data);
+          if (head==nullptr) {
+              head=temp;
+              tail=temp;
+          }else{
+            tail->next=temp;
+            temp->prev=tail;
+            tail=temp;
+          }
+          nodes++;
             // TODO
         }
-             
+
         void pop_front() {
+          auto* temp=head;
+          if(!head)
+              throw "Lista vacia";
+          if(nodes==1)
+              head=tail=nullptr;
+          else{
+              head->next->prev=nullptr;
+              head=temp->next;
+          }
+          delete temp;
+          nodes--;
             // TODO
         }
-             
+
         void pop_back() {
+          auto* temp=head;
+          if(!head)
+              throw "Lista vacia";
+          if(nodes==1)
+              head=tail=nullptr;
+          else{
+              while(temp->next!=tail)
+                  temp=temp->next;
+              tail=temp;
+              temp=temp->next;
+              tail->next=nullptr;
+          }
+          delete temp;
+          nodes--;
             // TODO
         }
-             
+
         iterator begin() {
+          return iterator(head);
             // TODO
         }
-             
+
         iterator end() {
+          return iterator(tail);
             // TODO
         }
-             
+
         ~DList() {
             if (head) {
                 head->killSelf();
-            } 
-        }         
+            }
+        }
 };
 
 #endif
--- slist.h
+++ slist.h
@@ -1,57 +1,127 @@
 #ifndef SLIST_H
 #define SLIST_H
-
+#include <iostream>
 #include "iterator.h"
-
+using namespace std;
 template <class T>
-class SListIterator : public Iterator<T> {     
-    public: 
+class SListIterator : public Iterator<T> {
+    public:
         SListIterator() : Iterator<T>() {};
         SListIterator(Node<T> *current) : Iterator<T>(current) {};
-        SListIterator<T> operator++();
+        SListIterator<T> operator++(){
+          this->current=this->current->next;
+          return *this;
+        };
 };
 
 template <typename Tr>
-class SList {     
+class SList {
     public:
         typedef typename Tr::T T;
         typedef typename Tr::Operation Operation;
         typedef SListIterator<T> iterator;
-      
+
     private:
         Node<T>* head;
         Operation cmp;
-              
+
     public:
         SList() {
             head = nullptr;
         };
 
         bool find(T search, Node<T> **&pointer) {
+          pointer=&head;
+          while(*pointer){
+            if(cmp(search,(*pointer)->data)){
+              break;
+            }
+            pointer=&((*pointer)->next);
+          }
+          if(!(*pointer)){
+            return false;
+          }
+          else if ((*pointer)->data==search) {
+            return true;
+          }
+          else{
+            return false;
+          }
+
             // TODO
         }
-             
+
         bool insert(T data) {
+          Node<T> **pointer;
+          bool x=find(data,pointer);
+          if(x){
+            return false;
+          }else{
+            auto* nodo= new Node<T>(data);
+            if (!*pointer) {
+              *pointer=nodo;
+              nodo->next=nullptr;
+              return true;
+            }
+            else{
+              auto* temp=*pointer;
+              *pointer=nodo;
+              nodo->next=temp;
+              return true;
+            }
+          }
             // TODO
         }
-             
+
         bool remove(T item) {
+          Node<T> **pointer;
+          bool x=find(item,pointer);
+          if(!x){
+            return false;
+          }else{
+            auto* temp=(*pointer)->next;
+            if(!temp){
+              temp=*pointer;
+              *pointer=nullptr;
+              delete temp;
+              return true;
+            }
+            else{
+              auto* temp2=*pointer;
+              *pointer=temp;
+              delete temp2;
+              return true;
+            }
+          }
             // TODO
-        }  
-             
+        }
+
         iterator begin() {
+          return iterator(head);
             // TODO
         }
-             
+
         iterator end() {
+          auto* temp=head;
+          while(temp)
+            temp=temp->next;
+          return iterator(temp);
             // TODO
         }
-             
+
         ~SList() {
             if (head) {
                 head->killSelf();
-            } 
-        }         
+            }
+        }
+      void print(){
+        auto* temp=head;
+        while(temp){
+          cout<<temp->data<<" ";
+          temp=temp->next;
+        }
+        cout<<" "<<endl;
+      }
 };
 
 #endif
